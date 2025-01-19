3565b61ccbe024f45a865d97a91b2192a41d0f42
MichaelStoll-unwsp <mastoll@students.unwsp.edu>
2025-01-18 19:13:20 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+
+print('Hello world')
+print('')
+print('“Do you see a man skillful in his work?')
+print('He will stand before kings;')
+print('he will not stand before obscure men.”')
+print(' (Proverbs 22:29)')
