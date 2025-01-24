97e5324a0970f84c096432b1f773fdddbc5259d4
MichaelStoll-unwsp <mastoll@students.unwsp.edu>
2025-01-24 09:17:42 -0600

    Add files via upload

--- program_1.py
+++ program_1.py
@@ -1,1 +1,5 @@
+#made by Michael Stoll
+#made 1/19/25
+#Hello World
+#(I forgot to add the needed comments, so I am reuploading the programs)
 print('Hello world')
--- program_2.py
+++ program_2.py
@@ -1,3 +1,7 @@
+#made by Michael Stoll
+#made 1/19/25
+#Hello World
+#(I forgot to add the needed comments, so I am reuploading the programs)
 print('“Do you see a man skillful in his work?')
 print('He will stand before kings;')
 print('he will not stand before obscure men.”')
